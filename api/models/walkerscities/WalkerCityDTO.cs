7d0ce750c6d96d1df86fe095db158e39b6eca9d8
mleelaw <mleelaw123@gmail.com>
2025-01-06 11:54:13 -0600

    filtering walkers by city logic with dropdown

---
+++ api/models/walkerscities/WalkerCityDTO.cs
@@ -0,0 +1,8 @@
+public class WalkerCityDTO
+{
+    public int WalkerCityId { get; set; }
+    public int WalkerId { get; set; }
+    public WalkerDTO Walker { get; set; }
+    public int CityId { get; set; }
+    public CityDTO City { get; set; }
+}
